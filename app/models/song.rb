345f28089c7349e416e2164d36d1b91469d530d6
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-11 22:14:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,11 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :release_year}
   validates :released, inclusion: { in: %w(true false)}
+
+  with_options if: :is_released?, length: { minimum: 50 } do
+  validates :content, if: -> { content.present? }
+
+  def is_released?
+  self.released
+end
 end
