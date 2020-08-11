be07b30de771db102c22bf5e0865e89dcbd6dc2b
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-11 22:37:46 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,6 +37,11 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
