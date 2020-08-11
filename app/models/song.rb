e15faeb3b3e09bccd784ec845d0840565ef47ca6
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-11 22:48:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :release_year}
+
   with_options if: :is_released? do |release|
     release.validates :release_year, presence: true, numericality: { less_than: Date.current.year }
   end
