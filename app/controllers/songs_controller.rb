1f952ba620efeeef6bf2c771ec6755e126f69e71
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-11 22:32:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,24 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create 
+    @song = Song.new(song_params)
+    if @song.valid? 
+      @song.save
+      redirect_to song_path(@song)
+    else 
+      render :new 
+    end
+  end
+
+  def show 
+    @song = Song.find(params[:id])
+  end
+
+  def edit 
+    @song = Song.find(params[:id])
+  end
+
+  
+
 end
